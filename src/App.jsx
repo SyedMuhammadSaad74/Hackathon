ca5b1202bbe1d204a8654a49e320e75c925094d8
Syed Muhammad Saad <136969885+SyedMuhammadSaad74@users.noreply.github.com>
2025-01-26 07:32:04 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import LoanRequestPage from './pages/LoanRequestPage';
 import UserDashboardPage from './pages/UserDashboardPage';
 import AdminDashboardPage from './pages/AdminDashboardPage';
 import Navbar from './components/Navbar';
-import Signup from './pages/Signup';
-import Signin from './pages/SignIn';
+
 import UpdateProfile from './pages/UpdateProfile';
 
 
@@ -19,8 +18,7 @@ const App = () => {
       <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
+
           <Route path="/update-profile" element={<UpdateProfile />} />
           <Route path="/loan-calculator" element={<LoanCalculatorPage />} />
           <Route path="/loan-request" element={<LoanRequestPage />} />
